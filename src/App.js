a27ac4a32d065d92408ef4564ee50b319e35f2b5
dev-langesh <dev.langesh@gmail.com>
2023-02-22 14:49:05 +0530

    counter forms listusers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Counter from "./components/Counter";
+import Form1 from "./components/Form";
+import ListUsers from "./components/ListUsers";
 import Welcome from "./components/Welcome";
 
 function App() {
-  return <Welcome name="langesh" text="Hello" />;
+  // return <Welcome name="langesh" text="Hello" />;
+
+  return (
+    <div>
+      <Counter />
+
+      <Form1 />
+
+      <ListUsers />
+    </div>
+  );
 }
 
 export default App;
